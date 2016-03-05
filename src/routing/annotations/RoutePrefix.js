4c35462bb75ed2ed7365c005e96130a49f363311
SondreB <pixelworld@hotmail.com>
2016-03-05 21:47:26 +0100

    Deletes js and map files from src.

--- src/routing/annotations/RoutePrefix.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require("reflect-metadata");
-function RoutePrefix(prefix) {
-    return function (target) {
-        Reflect.defineMetadata("RoutePrefix", prefix, target);
-    };
-}
-exports.RoutePrefix = RoutePrefix;
-//# sourceMappingURL=RoutePrefix.js.map
--- src/server/Startup.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=Startup.js.map
